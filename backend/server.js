2d2d5d8bf03aef3736be5b3788e9fdadebb73061
Feizo <felixkebaki@gmail.com>
2024-10-27 12:48:33 +0300

    env inclusion

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
-console.log("Hello")
+const express=require("express")
+const mongoose=require("mongoose")
+const dotenv=require("dotenv").config()
+
+const app=express()
+const PORT=5000;
+
+
+app.get("/",(req,res)=>{
+    res.status(200).json({message:"Hello"})
+})
+
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log("Connection to database was successful")
+    app.listen(PORT,()=>{
+        console.log(`Server listening to port ${PORT}...`)
+    })
+}).catch((error)=>{
+    console.log(error)
+})
